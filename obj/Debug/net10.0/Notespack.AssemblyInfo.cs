ca4593f5b0627667b068c7a12a10047a58fcedac
RosanaGarciaP <rdlagp@byui.edu>
2026-07-18 16:41:52 -0400

    Updating database

--- obj/Debug/net10.0/Notespack.AssemblyInfo.cs
+++ obj/Debug/net10.0/Notespack.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Notespack")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3bf9c47c14b620ebcbba1e727d4f575804708e7b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Notespack")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Notespack")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
